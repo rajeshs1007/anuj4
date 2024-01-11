d8dd173e2fd822a8e8160b0c26462613011030bc
AllComein <144424426+AllComein@users.noreply.github.com>
2024-01-11 12:31:20 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -16,13 +16,13 @@ const sequelize = new Sequelize(
   }
 );
 
-const db = {};
-db.Person = personModel(sequelize);
-db.sequelize = sequelize;
-// sync all models with database
-/*This checks what is the current state of the table in the database (which columns it has, 
-  what are their data types, etc),
- and then performs the necessary changes in the table to make it match the model.*/
-sequelize.sync({ alter: true });
+// const db = {};
+// db.Person = personModel(sequelize);
+// db.sequelize = sequelize;
+// // sync all models with database
+// /*This checks what is the current state of the table in the database (which columns it has, 
+//   what are their data types, etc),
+//  and then performs the necessary changes in the table to make it match the model.*/
+// sequelize.sync({ alter: true });
 
-module.exports = db;
+// module.exports = db;
