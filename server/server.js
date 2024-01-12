888e1a1ef98484bba40f07b7122e91859c1b0d5a
rajeshs1007 <149497527+rajeshs1007@users.noreply.github.com>
2024-01-12 11:41:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
+
 // const record = require("./controllers/person.controller")
 //parses incoming JSON data from the request body (post/put requests).
+const cors = require('cors');
+// Allow all origins
+app.use(cors());
+// Allow specific origin(s)
+app.use(cors({
+  origin: 'https://yourdeployedsite.com'
+}));
 app.use(express.json());
 //parse URL-encoded data from the request body.
 // URL-encoded data is commonly used when submitting HTML forms via POST requests.
 
 //The extended: true option allows for complex objects and arrays to be included in the form data.
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
 
 app.use("/record", require("./controllers/person.controller"));
 
